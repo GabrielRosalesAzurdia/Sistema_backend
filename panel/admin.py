2cfe11c938358e85ac6707561bda30dedd455037
GabrielRosalesAzurdia <gabrielalejandrorosalesazurdia@gmail.com>
2021-07-19 13:34:39 -0600

    registrando Work

--- panel/admin.py
+++ panel/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Grade, Class, Student, Score, Unit
+from .models import Grade, Class, Student, Score, Unit, Work
 
 admin.site.register(Grade)
 admin.site.register(Class)
 admin.site.register(Student)
 admin.site.register(Score)
 admin.site.register(Unit)
+admin.site.register(Work)
--- panel/models.py
+++ panel/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.conf import settings
 
+# tengo que trabajar en el backend 
+#         añadiendo el modelo de trabajos ya que scores es independiente
+
 class Grade(models.Model):
     name = models.CharField(max_length=250)
     teachers = models.ManyToManyField(settings.AUTH_USER_MODEL)
@@ -36,16 +39,24 @@ class Student(models.Model):
         grade_in_str = f'{self.grade.name}'
         return f'{self.name}' + " Grado: " + grade_in_str
 
-class Score(models.Model):
-    name = models.CharField(max_length=250, default= "")
-    description = models.CharField(max_length=250, default="")
-    score = models.IntegerField()
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
-    clase = models.ForeignKey('Class', on_delete=models.CASCADE)
+class Work(models.Model):
+    name = models.CharField(max_length=250)
+    grade = models.ForeignKey('Grade', on_delete=models.CASCADE)
+    classe = models.ForeignKey('Class', on_delete=models.CASCADE)
     unit = models.ForeignKey('Unit', on_delete=models.CASCADE)
 
     def __str__(self):
-        student_in_str = f'{self.student.name}'
-        clase_in_str = f'{self.clase.name}'
+        grade_in_str = f'{self.grade.name}'
+        clase_in_str = f'{self.classe.name}'
         unit_in_str = f'{self.unit.name}'
-        return f'{self.name}' + " Estudiante: " + student_in_str + " Clase: " + clase_in_str + " Unidad: " + unit_in_str 
+        return f'{self.name}' + " Grado: " + grade_in_str + " Clase: " + clase_in_str + " Unidad: " + unit_in_str 
+
+class Score(models.Model):
+    score = models.IntegerField()
+    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    work = models.ForeignKey('Work', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        student_in_str = f'{self.student.name}'
+        work_in_str = f'{self.work.name}'
+        return "Nota de trabajo de: " + work_in_str + " Estudiante: " + student_in_str
